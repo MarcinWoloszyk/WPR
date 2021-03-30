6965a882eaac57df0c6f35d678f5c505b64a95c1
MarcinWoloszyk <marcin1323101@wp.pl>
2021-03-30 23:32:51 +0200

    Pliki graficzne, kolejne zadanie

---
+++ Zadania/Zadanie3.php
@@ -0,0 +1,7 @@
+<html>
+<head>
+<title>Zadanie 3</title>
+</head>
+<body>
+</body>
+</html>
--- Zadania/wektory.php
+++ Zadania/wektory.php
@@ -4,7 +4,16 @@
 </head>
 <body>
 <?php
-    echo $_GET['x1'];
+    
+    $x1 = $_GET['x1'];
+    $y1 = $_GET['y1'];
+    $x2 = $_GET['x2'];
+    $y2 = $_GET['y2'];
+
+    $jeden = $x1 * $x2;
+    $dwa = $y1 * $y2;
+
+    echo $jeden + $dwa;
 
     
 ?>
